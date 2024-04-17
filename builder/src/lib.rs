598ed365a0c8e8052978f73e76b00e1db30c60ae
ungaro <alp.guneysel@gmail.com>
2024-04-17 19:55:48 -0400

    builder macro - pass test 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,33 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input,  DeriveInput};
-
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    //let name= &ast.ident;
-    eprintln!("Debuging_here {:?}", ast);
-    TokenStream::new()
+    //eprintln!("Debuging_here {:?}", ast);
+
+    let name = &ast.ident;
+
+    let builder_name = format!("{}Builder", name);
+
+    //get the original name's span to use in the new Ident
+    let builder_ident = Ident::new(&builder_name, name.span());
+    
+
+    let expanded = quote!{
+        struct #builder_ident {
+         
+        }
+
+        impl #name {
+ 
+            pub fn builder() -> #builder_ident {
+                #builder_ident{}
+            }
+        }
+    };
+    expanded.into()
+
+    //TokenStream::new()
 }
