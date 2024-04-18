4ce335c5cf6e6cff0e03bc046e2471ef5bb026e1
ungaro <alp.guneysel@gmail.com>
2024-04-17 20:10:56 -0400

    cargo expand & finish test2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -13,17 +13,23 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     //get the original name's span to use in the new Ident
     let builder_ident = Ident::new(&builder_name, name.span());
-    
 
-    let expanded = quote!{
+    let expanded = quote! {
         struct #builder_ident {
-         
+         executable: Option<String>,
+         args: Option<Vec<String>>,
+         env: Option<Vec<String>>,
+         current_dir: Option<String>,
         }
 
         impl #name {
- 
             pub fn builder() -> #builder_ident {
-                #builder_ident{}
+                #builder_ident{
+                executable: None,
+                 args: None,
+                 env: None,
+                 current_dir: None,
+                }
             }
         }
     };
